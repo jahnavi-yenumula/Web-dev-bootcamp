4ac6537fc363aad09f54691879ef59f3e737541c
jahnavi-yenumula <16jahnavi0307@gmail.com>
2026-08-13 16:30:48 +0530

    added toggle user functionality

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,23 @@
 // alert("welcome to my web page");
+var Users=[
+    {
+        "name":"John doe",
+        "gender": "Male",
+        "image": "john.png"
+    },
+    {
+        "name":"Jane doe",
+        "gender": "Female",
+        "image": "jane.png"
+    }
+]
+var curId=0;
+function toggleUser(){
+    curId=1-curId;
+    var userName=document.getElementById("user-name");
+    var userImage=document.getElementById("user-image");
+    var userGender=document.getElementById("user-gender");
+    userName.innerHTML=Users[curId].name;
+    userImage.src=Users[curId].image;
+    userGender.innerHTML=Users[curId].gender;
+}
