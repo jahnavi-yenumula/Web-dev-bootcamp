f849117566a5a55076a52656db0134fb8076c897
jahnavi-yenumula <16jahnavi0307@gmail.com>
2026-08-14 10:47:37 +0530

    added random user functionality

--- frontend/index.js
+++ frontend/index.js
@@ -21,3 +21,21 @@ function toggleUser(){
     userImage.src=Users[curId].image;
     userGender.innerHTML=Users[curId].gender;
 }
+function randomUser(){
+    fetch("https://randomuser.me/api/")
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        var userName=document.getElementById("user-name");
+        var userImage=document.getElementById("user-image");
+        var userGender=document.getElementById("user-gender");
+        userName.innerHTML=data.results[0].name.first + " " +data.results[0].name.last;
+        userGender.innerHTML=data.results[0].gender;
+        userImage.src=data.results[0].picture.medium;
+
+    })
+    .catch(function(err){
+        console.log("error ouccured: "+err);
+    })
+}
